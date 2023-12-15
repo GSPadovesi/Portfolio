e7b6ae40f53a8760a10e7e8cd47d4c6ffb82c00e
Gabriel <gspadovesi@gmail.com>
2023-12-15 16:44:24 -0300

    Termino depois

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
 import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
+import gsap from 'gsap'
 import * as THREE from 'three';
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 15;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
@@ -19,19 +21,39 @@ document.body.appendChild(renderer.domElement);
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.ShaderMaterial({ vertexShader, fragmentShader, uniforms: { globeTexture: { value: new THREE.TextureLoader().load(Earth) } } }));
 const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.ShaderMaterial({ vertexShader: atmosphereVertexShader, fragmentShader: atmosphereFragmentShader, blending: THREE.AdditiveBlending, side: THREE.BackSide }));
 atmosphere.scale.set(1.1, 1.1, 1.1);
+const group = new THREE.Group();
 
-scene.add(sphere, atmosphere);
+scene.add(atmosphere)
+group.add(sphere);
+scene.add(group);
+// scene.add(sphere, atmosphere);
 
-camera.position.z = 15;
+const mouse = {
+  x: 0,
+  y: 0
+}
+
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  sphere.rotation.y += 0.001;
+  sphere.rotation.y += 0.004;
+  // group.rotation.y = mouse.x * 0.5
+  gsap.to(group.rotation, {
+    x: -mouse.y * 0.5,
+    y: mouse.x * 0.5,
+    duration: 2
+  })
 };
 
 animate();
 
+
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+  console.log(mouse)
+})
 
 
 
