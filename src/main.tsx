04f75c41a14b888dbc14c5271a4559b5d091fb23
Gabriel <gspadovesi@gmail.com>
2023-12-15 16:44:31 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,9 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 15;
-
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
+camera.position.z = 15;
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
@@ -20,13 +19,28 @@ document.body.appendChild(renderer.domElement);
 //Criando sphere
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.ShaderMaterial({ vertexShader, fragmentShader, uniforms: { globeTexture: { value: new THREE.TextureLoader().load(Earth) } } }));
 const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.ShaderMaterial({ vertexShader: atmosphereVertexShader, fragmentShader: atmosphereFragmentShader, blending: THREE.AdditiveBlending, side: THREE.BackSide }));
-atmosphere.scale.set(1.1, 1.1, 1.1);
 const group = new THREE.Group();
 
+atmosphere.scale.set(1.1, 1.1, 1.1);
 scene.add(atmosphere)
 group.add(sphere);
 scene.add(group);
 // scene.add(sphere, atmosphere);
+
+const starGeometry = new THREE.BufferGeometry();
+const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
+const stars = new THREE.Points(starGeometry, starMaterial);
+const starVertices = [];
+
+for (let i = 0; i < 10000; i++) {
+  const x = (Math.random() - 0.5) * 2000;
+  const y = (Math.random() - 0.5) * 2000;
+  const z = -Math.random() * 2000;
+  starVertices.push(x, y, z);
+}
+
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3));
+scene.add(stars);
 
 const mouse = {
   x: 0,
@@ -38,7 +52,6 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   sphere.rotation.y += 0.004;
-  // group.rotation.y = mouse.x * 0.5
   gsap.to(group.rotation, {
     x: -mouse.y * 0.5,
     y: mouse.x * 0.5,
@@ -52,7 +65,6 @@ animate();
 addEventListener('mousemove', (event) => {
   mouse.x = (event.clientX / innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / innerHeight) * 2 + 1;
-  console.log(mouse)
 })
 
 
