89b539d21825a1747e3751bff7cc1215a893f107
Gabriel <gspadovesi@gmail.com>
2023-12-21 13:43:37 -0300

    Depois melhoro o código

--- src/pages/Main/Section/Skills/Skills.tsx
+++ src/pages/Main/Section/Skills/Skills.tsx
@@ -2,34 +2,18 @@ import { Iconcircle } from '../../../../components/Iconcircle/Iconcircle';
 import { SkillsProps } from './Skills.types';
 import { ThreeComponent } from '../../../../components/Earth/Earth';
 import JS from '../../../../assets/js.png';
+import HTML from '../../../../assets/html-5.png';
+import CSS from '../../../../assets/css-3.png';
+import TS from '../../../../assets/TS.png';
+import JAVA from '../../../../assets/java.png';
+import SQL from '../../../../assets/mysql.png';
+import AFRAME from '../../../../assets/aframe.png';
+import MONGODB from '../../../../assets/mongodb.png';
+import GRAPHQL from '../../../../assets/graphql.png';
 import * as S from './Skills.styles';
 
 let Skill = [{
-  row: [
-    {
-      title: 'Javascript',
-      subtitle: 'Programação',
-      image: JS,
-    },
-    {
-      title: 'Javascript',
-      subtitle: 'Programação',
-      image: JS,
-    },
-  ],
-  row2: [
-    {
-      title: 'Javascript',
-      subtitle: 'Programação',
-      image: JS,
-    },
-    {
-      title: 'Javascript',
-      subtitle: 'Programação',
-      image: JS,
-    },
 
-  ]
 }]
 
 export const Skills = ({ isChecked }: SkillsProps) => {
@@ -41,33 +25,21 @@ export const Skills = ({ isChecked }: SkillsProps) => {
       <S.SkillsContainer>
         <S.SkillsItems>
           <S.SkillsTitle>Habilidades</S.SkillsTitle>
-
-          {Skill.map((item, index) => (
-            <S.SkillsItem key={index}>
-              {item.row.map((item, index) => (
-                <Iconcircle key={index} isChecked={isChecked} image={item.image} title={item.title} subtitle={item.subtitle} />
-              ))}
-            </S.SkillsItem>
-
-          ))}
-          {/* {
-            Skill.map((item, index) => {
-              return (
-                <S.SkillsItem key={index}>
-                  {item.row.map((item, index) => {
-                    return (
-                      <Iconcircle key={index} isChecked={isChecked} image={item.image} title={item.title} subtitle={item.subtitle} />
-                    )
-                  })}
-                </S.SkillsItem>
-              )
-            })
-          } */}
-
-          {/* <S.SkillsItem>
-            <Iconcircle isChecked={isChecked} image={JS} title='Javascript' subtitle='Programação' />
-            <Iconcircle isChecked={isChecked} image={JS} title='Javascript' subtitle='Programação' />
-          </S.SkillsItem> */}
+          <S.SkillsItem>
+            <Iconcircle isChecked={isChecked} image={JS} title='Javascript' subtitle='Linguagem de programação' />
+            <Iconcircle isChecked={isChecked} image={HTML} title='HTML5' subtitle='Linguagem de marcação' />
+            <Iconcircle isChecked={isChecked} image={CSS} title='CSS3' subtitle='Cascading Style Sheets' />
+          </S.SkillsItem>
+          <S.SkillsItem>
+            <Iconcircle isChecked={isChecked} image={TS} title='Typescript' subtitle='Linguagem de programação' />
+            <Iconcircle isChecked={isChecked} image={JAVA} title='Java' subtitle='Linguagem de programação' />
+            <Iconcircle isChecked={isChecked} image={SQL} title='MySQL' subtitle='Linguagem SQL' />
+          </S.SkillsItem>
+          <S.SkillsItem>
+            <Iconcircle isChecked={isChecked} image={AFRAME} title='Aframe' subtitle='Linguagem de programação / 3D' />
+            <Iconcircle isChecked={isChecked} image={MONGODB} title='MongoDB' subtitle='Banco de dados não relacional' />
+            <Iconcircle isChecked={isChecked} image={GRAPHQL} title='GraphQL' subtitle='Linguagem de consulta' />
+          </S.SkillsItem>
         </S.SkillsItems>
         <S.SkillsCanva>
           <ThreeComponent />
